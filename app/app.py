c4b3abf008750b8402f194afeb1d1923fdf0564e
shivammarkanday <shivammarkandaystudy@gmail.com>
2025-11-11 14:16:59 +0530

    SCROOOOOOOGE

--- app/app.py
+++ app/app.py
@@ -69,9 +69,10 @@ col1, col2 = st.columns([3,1])
 with col1:
     st.markdown('<div class="big-title">🚨 Credit Card Fraud Detection</div>', unsafe_allow_html=True)
     st.write("Interactive demo — upload transactions or test single records. Model: Random Forest.")
+    img_path = "img/scrooge_mcduck_s_face_by_adrianapendleton_detahh0.png"
 with col2:
     # optional image if present
-    img_path = "app/img/scrooge_mcduck_s_face_by_adrianapendleton_detahh0.png"
+    # img_path = "/Users/shivammarkanday/Library/CloudStorage/OneDrive-AdaniUniversity/SEMESTERS/SEM 5 (2025)/Machine Learning (ML)/Project/fraud_detection/img/scrooge_mcduck_s_face_by_adrianapendleton_detahh0.png"
     if os.path.exists(img_path):
         st.image(img_path, width=60)
 
